ecd197efdf4b6652bf792a2c3527925798d90dc6
SaraBarrett <sara.c.mon@gmail.com>
2024-02-15 17:03:07 +0000

    image

--- resources/views/layouts/femaster.blade.php
+++ resources/views/layouts/femaster.blade.php
@@ -16,7 +16,8 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#"> <img width="50px" height="50px"
+                    src="{{ asset('images/lara.jpg') }}" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
